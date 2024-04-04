35cb30bb3fa92e611b56adb12bd2e43509b7a2a4
Mihai Manole <mesterum@users.noreply.github.com>
2024-04-04 19:11:52 +0000

    init

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    // https://blog.devgenius.io/how-to-deploy-your-vite-react-app-to-github-pages-with-and-without-react-router-b060d912b10e
+    config.base = '/goit-react-hw-01-components/'
+  }
+
+  return config
+})
